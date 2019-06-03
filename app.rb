4dbd2f6c695b2e54550d4bc947fb2fcfff17445e
Joel Seng <joel29dec@gmail.com>
2019-06-03 10:15:44 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
-
+require 'sinatra'
 class App < Sinatra::Base
+
+
+    get '/' do
+        "Hello, World!"
+      end
+
+    get '/name' do
+        "My name is Joel"
+    end
+
+    get '/hometown' do
+        "My hometown is Singapore"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Hey Jude"
+    end
+
 end
